093131967bc69bc2ec99ed1c10d7f522492926e1
Jess <kiwifails@users.noreply.github.com>
2020-07-19 20:22:15 -0400

    Added Half Panels!
    These are super neat

--- src/main/java/com/kiwi/elysian/blocks/Whitestone.java
+++ src/main/java/com/kiwi/elysian/blocks/Whitestone.java
@@ -4,6 +4,7 @@ import com.google.common.collect.Lists;
 import com.kiwi.elysian.Elysian;
 import com.kiwi.elysian.util.CustomPillar;
 import com.kiwi.elysian.util.CustomStairs;
+import com.kiwi.elysian.util.PanelHalf;
 import com.kiwi.elysian.util.PanelReg;
 import net.minecraft.block.Block;
 import net.minecraft.block.SlabBlock;
@@ -25,7 +26,7 @@ public class Whitestone {
 
     public static Block WHITESTONE = register("whitestone", new Block(Block.Properties.create(Material.ROCK, MaterialColor.SNOW).hardnessAndResistance(1.5f, 6.0f).sound(SoundType.STONE).func_235861_h_().harvestTool(ToolType.PICKAXE).harvestLevel(0)));
     public static Block WHITESTONE_COBBLED = register("whitestone_cobbled", new Block(Block.Properties.create(Material.ROCK, MaterialColor.SNOW).hardnessAndResistance(2.0f, 6.0f).sound(SoundType.STONE).func_235861_h_().harvestTool(ToolType.PICKAXE).harvestLevel(0)));
-    public static Block WHITESTONE_POLISHED = register("whitestone_polished", new Block(Block.Properties.create(Material.ROCK, MaterialColor.SNOW).hardnessAndResistance(1.5f, 6.0f).sound(SoundType.STONE).func_235861_h_().harvestTool(ToolType.PICKAXE).harvestLevel(0)));
+    public static Block WHITESTONE_POLISHED = register("whitestone_polished", new Block(Block.Properties.create(Material.ROCK, MaterialColor.SNOW).hardnessAndResistance(2.0f, 6.0f).sound(SoundType.STONE).func_235861_h_().harvestTool(ToolType.PICKAXE).harvestLevel(0)));
     public static Block WHITESTONE_BRICK = register("whitestone_brick", new Block(Block.Properties.create(Material.ROCK, MaterialColor.SNOW).hardnessAndResistance(1.5f, 6.0f).sound(SoundType.STONE).func_235861_h_().harvestTool(ToolType.PICKAXE).harvestLevel(0)));
 
     public static final Block WHITESTONE_SLAB = register("whitestone_slab", new SlabBlock(Block.Properties.from(WHITESTONE)));
@@ -37,6 +38,8 @@ public class Whitestone {
     public static final Block WHITESTONE_PILLAR = register("whitestone_pillar", new CustomPillar(Block.Properties.from(WHITESTONE)));
 
     public static final Block WHITESTONE_PANEL = register("whitestone_panel", new PanelReg(Block.Properties.from(WHITESTONE)));
+
+    public static final Block WHITESTONE_HALF_PANEL = register("whitestone_half_panel", new PanelHalf(Block.Properties.from(WHITESTONE)));
 
 
     private static Block register(String registryName, Block thisBlock) {
