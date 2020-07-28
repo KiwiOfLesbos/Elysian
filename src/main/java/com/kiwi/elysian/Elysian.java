ae247469bb020ece30d19312eca174535b2550df
Jess <kiwifails@users.noreply.github.com>
2020-07-28 01:11:41 -0400

    just ugh

--- src/main/java/com/kiwi/elysian/Elysian.java
+++ src/main/java/com/kiwi/elysian/Elysian.java
@@ -1,9 +1,15 @@
 package com.kiwi.elysian;
 
 import com.kiwi.elysian.blocks.Whitestone;
+import com.kiwi.elysian.data.DataGenerators;
+import com.kiwi.elysian.util.SideProxy;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.*;
+import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -13,11 +19,37 @@ public class Elysian {
     public static final String MODID = "elysian";
     public static final Logger LOGGER = LogManager.getLogger();
 
+
     public static final ItemGroup TAB = new ItemGroup("elysianTab") {
         @Override
         public ItemStack createIcon() {
             return new ItemStack(Whitestone.WHITESTONE_BRICK);
         }
     };
+
+    Elysian() {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::commonSetup);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::imcEnqueue);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::imcProcess);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(DataGenerators::gatherData);
+
+        MinecraftForge.EVENT_BUS.addListener(this::onServerStarting);
+    }
+
+    private void commonSetup(FMLCommonSetupEvent event) {
+        Elysian.LOGGER.debug("Common Setup");
+    }
+
+    private void onServerStarting(FMLServerStartingEvent event) {
+
+    }
+
+    private void imcEnqueue(InterModEnqueueEvent event) {
+    }
+
+    private void imcProcess(InterModProcessEvent event) {
+    }
+
+
 
 }
--- src/main/java/com/kiwi/elysian/util/SideProxy.java
+++ src/main/java/com/kiwi/elysian/util/SideProxy.java
@@ -3,9 +3,7 @@ package com.kiwi.elysian.util;
 import com.kiwi.elysian.Elysian;
 import com.kiwi.elysian.data.DataGenerators;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLDedicatedServerSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.*;
 import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
@@ -13,18 +11,26 @@ public class SideProxy {
 
     SideProxy() {
 
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(SideProxy::commonSetup);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::commonSetup);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::imcEnqueue);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::imcProcess);
         FMLJavaModLoadingContext.get().getModEventBus().addListener(DataGenerators::gatherData);
 
         MinecraftForge.EVENT_BUS.addListener(SideProxy::serverStarting);
     }
 
-    private static void commonSetup(FMLCommonSetupEvent event) {
+    private void commonSetup(FMLCommonSetupEvent event) {
         Elysian.LOGGER.debug("Common Setup");
     }
 
     private static void serverStarting(FMLServerStartingEvent event) {
 
+    }
+
+    private void imcEnqueue(InterModEnqueueEvent event) {
+    }
+
+    private void imcProcess(InterModProcessEvent event) {
     }
 
     static class Client extends SideProxy {
@@ -45,7 +51,5 @@ public class SideProxy {
 
         }
     }
-
-
 
 }
